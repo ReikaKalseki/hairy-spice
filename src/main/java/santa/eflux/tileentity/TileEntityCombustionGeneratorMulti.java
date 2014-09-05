9687f2d53596785cd6dc3a0e6343737514f387c7
SatanicSanta <satanicsantagaming@gmail.com>
2014-09-04 19:32:19 -0700

    removed broken code

--- src/main/java/santa/eflux/tileentity/TileEntityCombustionGeneratorMulti.java
+++ src/main/java/santa/eflux/tileentity/TileEntityCombustionGeneratorMulti.java
@@ -5,62 +5,15 @@ import net.minecraft.tileentity.TileEntity;
 
 
 public class TileEntityCombustionGeneratorMulti extends TileEntity {
-    private boolean hasMaster, isMaster;
-    private int masterX, masterY, masterZ;
 
     @Override
     public void writeToNBT(NBTTagCompound data){
         super.writeToNBT(data);
-        data.setInteger("masterX", masterX);
-        data.setInteger("masterY", masterY);
-        data.setInteger("masterZ", masterZ);
-        data.setBoolean("hasMaster", hasMaster);
-        data.setBoolean("isMaster", isMaster);
     }
 
     @Override
     public void readFromNBT(NBTTagCompound data){
         super.readFromNBT(data);
-        data.setInteger("masterX", masterX);
-        data.setInteger("masterY", masterY);
-        data.setInteger("masterZ", masterZ);
-        data.setBoolean("hasMaster", hasMaster);
-        data.setBoolean("isMaster", isMaster);
     }
-
-    public boolean haMaster(){
-        return hasMaster;
-    }
-
-    public boolean isMaster(){
-        return isMaster;
-    }
-
-    public int getMasterX(){
-        return masterX;
-    }
-
-    public int getMasterY(){
-        return masterY;
-    }
-
-    public int getMasterZ(){
-        return masterY;
-    }
-
-    public void setHasMaster(boolean bool){
-        hasMaster = bool;
-    }
-
-    public void setIsMaster(boolean bool){
-        isMaster = bool;
-    }
-
-    public void setMasterCoords(int x, int y, int z){
-        masterX = x;
-        masterY = y;
-        masterZ = z;
-    }
-
 
 }
