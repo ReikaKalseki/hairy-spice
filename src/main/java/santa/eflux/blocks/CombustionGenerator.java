b1284ae296606c6b9307f65d774d2b27c28232a5
SatanicSanta <elisinsane@yahoo.com>
2014-09-04 22:11:28 -0700

    Update CombustionGenerator.java

--- src/main/java/santa/eflux/blocks/CombustionGenerator.java
+++ src/main/java/santa/eflux/blocks/CombustionGenerator.java
@@ -3,24 +3,50 @@ package santa.eflux.blocks;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import santa.eflux.EnergeticFluxMain;
 import santa.eflux.reference.Reference;
 import santa.eflux.tileentity.TileEntityCombustionGenerator;
+import santa.eflux.tileentity.TileEntityCombustionGeneratorMulti;
 
 import java.util.Random;
 
 
-public class CombustionGenerator extends BlockEflux {
+public class CombustionGenerator extends BlockContainer {
 
     public CombustionGenerator(){
-        super(Material.iron, 1.5F, 10.0F, soundTypeMetal, BlockInfo.COMBUST_UNLOCALIZED, Reference.EFLUX_TAB);
+        super(Material.iron);
+        this.setCreativeTab(Reference.EFLUX_TAB);
+        this.setBlockName(BlockInfo.COMBUST_UNLOCALIZED);
+        this.setHardness(1.5F);
+        this.setResistance(10.0F);
+        this.setStepSound(soundTypeMetal);
+        //super(Material.iron, 1.5F, 10.0F, soundTypeMetal, BlockInfo.COMBUST_UNLOCALIZED, Reference.EFLUX_TAB);
     }
 
     @Override
-    public TileEntity createTileEntity(World world, int meta){
-            return new TileEntityCombustionGenerator();
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block block){
+        TileEntity tile = world.getTileEntity(x, y, z);
+        if (tile != null && tile instanceof TileEntityCombustionGeneratorMulti){
+            TileEntityCombustionGeneratorMulti multiblock = (TileEntityCombustionGeneratorMulti) tile;
+            if (multiblock.hasMaster()){
+                if (!multiblock.checkMultiBlockForm())
+                    multiblock.resetStructure();
+            } else {
+                if (!multiblock.checkForMaster()){
+                    multiblock.reset();
+                    world.markBlockForUpdate(x, y, z);
+                }
+            }
+        }
+        super.onNeighborBlockChange(world, x, y, z, block);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int meta){
+        return new TileEntityCombustionGeneratorMulti();
     }
 }
