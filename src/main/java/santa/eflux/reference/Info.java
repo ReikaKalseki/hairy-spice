c74e0519c3b14b8eb65855e31332be763144e346
Strikingwolf <Strikingwolf2012@gmail.com>
2014-09-06 10:06:17 -0500

    Adding Fluids to info file

--- src/main/java/santa/eflux/reference/Info.java
+++ src/main/java/santa/eflux/reference/Info.java
@@ -4,10 +4,12 @@ public final class Info
 {
     public static final class Items
     {
+        //Hardened Iron
         public static final String IRON_UNLOCALIZED = "ironHard";
         public static final String IRON = "hardened_iron";
         public static final String IRON_KEY = "ironHard";
 
+        //Hammer
         public static final String HAMMER_UNLOCALIZED = "efluxHammer";
         public static final String HAMMER = "hammer";
         public static final String HAMMER_KEY = "efluxHammer";
@@ -16,22 +18,36 @@ public final class Info
 
     public static final class Blocks
     {
+        //Combustion Generator Controller
         public static final String COMBUST_UNLOCALIZED = "generatorCombust";
         public static final String COMBUST = "combustion_generator";
         public static final String COMBUST_KEY = "combustion_generator";
 
+        //Generator Base
         public static final String GENBASE_UNLOCALIZED = "generatorBase";
         public static final String GENBASE = "generator_base";
         public static final String GENBASE_KEY = "generator_base";
 
+        //Tank
         public static final String TANK_UNLOCALIZED = "tank";
         public static final String TANK = "tank";
         public static final String TANK_KEY = "tank";
 
+        //Compressor
         public static final String COMPRESSOR_UNLOCALIZED = "metalCompressor";
         public static final String COMPRESSOR = "metal_compressor";
         public static final String COMPRESSOR_KEY = "metal_compressor";
     }
 
+    public static final class Fluids
+    {
 
+        //Steam
+        public static final String STEAM_NAME = "Steam";
+        public static final String STEAM_BLOCK_NAME = "Steam";
+        public static final int STEAM_LUMINOSITY = 0;
+        public static final int STEAM_TEMPERATURE = 100;
+        public static final int STEAM_VISCOSITY = 1500;
+        public static final boolean STEAM_IS_GASEOUS = true;
+    }
 }
