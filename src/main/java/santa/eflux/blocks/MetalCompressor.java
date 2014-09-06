432bdef66cb92a31852b31df3fe4a17439638531
Strikingwolf <Strikingwolf2012@gmail.com>
2014-09-05 21:40:16 -0500

    Add new b/c Santa forgot it

--- src/main/java/santa/eflux/blocks/MetalCompressor.java
+++ src/main/java/santa/eflux/blocks/MetalCompressor.java
@@ -7,20 +7,18 @@ import net.minecraft.world.World;
 import santa.eflux.reference.Reference;
 import santa.eflux.tileentity.TileEntityMetalCompressor;
 
-
-/**
- * Created by elijahfoster-wysocki on 9/5/14.
- */
 public class MetalCompressor extends BlockEflux{
 
     public MetalCompressor(){
         super(Material.iron, 1.5F, 10.0F, soundTypeMetal, BlockInfo.COMPRESSOR_UNLOCALIZED, Reference.EFLUX_TAB);
     }
 
+
     @Override
     public TileEntity createTileEntity(World world, int meta){
-        return TileEntityMetalCompressor();
+        return new TileEntityMetalCompressor();
     }
+
 
     @Override
     public int getLightValue(IBlockAccess world, int x, int y, int z) {
