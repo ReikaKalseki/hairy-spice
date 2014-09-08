cd11e94889e1647f2af415a1d510b4b45e2f22aa
Strikingwolf <Strikingwolf2012@gmail.com>
2014-09-07 19:44:16 -0500

    More tiny things

--- src/main/java/santa/api/interfaces/wrench/IWrench.java
+++ src/main/java/santa/api/interfaces/wrench/IWrench.java
@@ -2,7 +2,7 @@ package santa.api.interfaces.wrench;
 
 public interface IWrench
 {
-    //For this to work you must insert this code into your wrench class
+    //For this to work you must insert this code into your wrench class and implement isWrench
     /*
     @Override
     public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
